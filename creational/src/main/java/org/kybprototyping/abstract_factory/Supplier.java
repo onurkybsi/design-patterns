da097f5cccdfb41cc1139df2ba17aafd2cdb5853
Onur Kayabasi <onurbpm@outlook.com>
2022-11-09 01:08:10 +0300

    Abstract Factory imp completely done

--- creational/src/main/java/org/kybprototyping/abstract_factory/Supplier.java
+++ creational/src/main/java/org/kybprototyping/abstract_factory/Supplier.java
@@ -3,12 +3,15 @@ package org.kybprototyping.abstract_factory;
 import org.kybprototyping.abstract_factory.impl.apple.AppleFactory;
 import org.kybprototyping.abstract_factory.impl.samsung.SamsungFactory;
 
+// Client needs only to communicate with factory provider to access manufacturer products
 public class Supplier {
   private static final String UNSUPPORTED_BRANCD_EXCEPTION_MESSAGE = "brand can only be \"Apple\" or \"Samsung\"!";
 
   private AppleFactory appleFactory = new AppleFactory();
   private SamsungFactory samsungFactory = new SamsungFactory();
 
+  // Client can choose itself the created instance through some configuration
+  // parameters(brand in this case)
   public CellPhone getCellPhone(Brand brand) {
     return switch (brand) {
       case APPLE -> appleFactory.getCellPhone();
@@ -25,6 +28,9 @@ public class Supplier {
     };
   }
 
+  // Client also can get the factory itself by some configurations.
+  // So, it can construct the objects from the same family.
+  // For example: Iphone & AppleChargingCable
   public ManufacturerAbstractFactory getFactory(Brand brand) {
     return switch (brand) {
       case APPLE -> appleFactory;
