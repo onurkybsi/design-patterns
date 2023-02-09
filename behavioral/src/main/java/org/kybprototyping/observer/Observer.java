aad7628c26cea1eb627a174221d3bcdef646d5d9
Onur Kayabasi <onurbpm@outlook.com>
2023-02-09 22:45:16 +0100

    Continue to Observer

--- behavioral/src/main/java/org/kybprototyping/observer/Observer.java
+++ behavioral/src/main/java/org/kybprototyping/observer/Observer.java
@@ -1,7 +1,15 @@
 package org.kybprototyping.observer;
 
+/**
+ * Represents the interface which the observer components should support
+ */
 public interface Observer<T> {
 
+	/**
+	 * Updates the observer state by the change in the subject
+	 * 
+	 * @param state current state information of the observed subject
+	 */
 	void update(T state);
 
 }
--- behavioral/src/main/java/org/kybprototyping/observer/Subject.java
+++ behavioral/src/main/java/org/kybprototyping/observer/Subject.java
@@ -1,11 +1,33 @@
 package org.kybprototyping.observer;
 
+/**
+ * Represents the interface which the subject componenets should support
+ * 
+ * @author Onur Kayabasi
+ */
 public interface Subject<T> {
 
+	/**
+	 * Registers the given {@code observer} to the subject to be notified when the {@code <T>} state
+	 * is changed.
+	 * 
+	 * @param observer observer to observe the changes in the subject
+	 */
 	void attach(Observer<T> observer);
 
+	/**
+	 * Unregisters the given {@code observer} from the subject. So, the {@code observer} is not
+	 * notified when the {@code <T>} state is changed.
+	 * 
+	 * @param observer registered observer which is not going to be notified anymore
+	 */
 	void deattach(Observer<T> observer);
 
+	/**
+	 * Notifies the registered observers that the state has been changed.
+	 * 
+	 * @param state new state
+	 */
 	void notify(T state);
 
 }
